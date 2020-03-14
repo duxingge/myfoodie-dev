9e3b0dad10334823cfdc97d15c1065e8ee3df31e
wjx <156717494@qq.com>
2020-03-14 19:50:30 +0800

    添加地址管理模块接口

--- myfoodie-dev-api/src/main/java/com/example/controller/BaseController.java
+++ myfoodie-dev-api/src/main/java/com/example/controller/BaseController.java
@@ -1,10 +1,5 @@
 package com.example.controller;
 
-import com.example.pojo.Stu;
-import com.example.service.StuService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
